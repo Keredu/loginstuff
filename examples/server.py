67507160114151eb37e0f7efb61138e6bd1e0cbe
Keredu <luiscortesferre@gmail.com>
2023-10-04 21:47:25 +0200

    register works but login dont

--- examples/server.py
+++ examples/server.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI, HTTPException, Form
+from fastapi import FastAPI, HTTPException, Body
+from fastapi.responses import FileResponse
 import bcrypt
 import sqlite3
 import os
-from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
-# This will allow serving of any static file from the current directory
-app.mount("/", StaticFiles(directory="."), name="static")
+# Establish a connection to the database
+DB_NAME = './users.db'
+db = sqlite3.connect(DB_NAME, check_same_thread=False)
 
-db = sqlite3.connect('./users.db', check_same_thread=False)
-
-def run_query(db, query, params=()):
+def run_query(query, params=()):
     cursor = db.cursor()
     cursor.execute(query, params)
     db.commit()
@@ -19,14 +19,14 @@ def run_query(db, query, params=()):
 
 async def addUser(username, password):
     hashedPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    run_query(db, "INSERT INTO user(username, password) VALUES (?, ?)", (username, hashedPassword.decode('utf-8')))
+    run_query("INSERT INTO user(username, password) VALUES (?, ?)", (username, hashedPassword.decode('utf-8')))
 
 def getUserByName(name):
-    cursor = run_query(db, "SELECT * FROM user WHERE username = ?", (name,))
+    cursor = run_query("SELECT * FROM user WHERE username = ?", (name,))
     return cursor.fetchone()
 
 @app.post("/login")
-async def login(username: str = Form(...), password: str = Form(...)):
+async def login(username: str = Body(...), password: str = Body(...)):
     user = getUserByName(username)
     if not user:
         raise HTTPException(status_code=400, detail="User not found.")
@@ -36,23 +36,29 @@ async def login(username: str = Form(...), password: str = Form(...)):
         raise HTTPException(status_code=400, detail="Invalid password.")
 
 @app.post("/register")
-async def register(username: str = Form(...), password: str = Form(...)):
+async def register(username: str = Body(...), password: str = Body(...)):
     try:
         await addUser(username, password)
         return {"message": "User registered successfully!"}
+    except sqlite3.IntegrityError:
+        raise HTTPException(status_code=400, detail="Username already taken.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@app.get("/")
+def serve_root():
+    return FileResponse("index.html")
+
+@app.get("/{filename:path}")
+def serve_file(filename: str):
+    return FileResponse(filename)
+
 if __name__ == "__main__":
-    DB_NAME = './users.db'
-    if os.path.exists(DB_NAME):
-        os.remove(DB_NAME)
-    try:
-        run_query(db, "CREATE TABLE IF NOT EXISTS user (username TEXT, password TEXT)")
-    except Exception as e:
-        print(f"Failed to create table: {e}")
+    if not os.path.exists(DB_NAME):
+        try:
+            run_query("CREATE TABLE IF NOT EXISTS user (username TEXT PRIMARY KEY, password TEXT)")
+        except Exception as e:
+            print(f"Failed to create table: {e}")
     else:
         import uvicorn
         uvicorn.run(app, host="0.0.0.0", port=3000)
-
-    # NOTE: Not working 100% (the alerts show undefined)
